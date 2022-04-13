5ec082ba531fa4de7539af53f167b7e9392904d2
ZarTek Creole <zartek.creole@gmail.com>
2022-04-13 02:47:59 +0000

    test run slftp system

--- locale/make_mo.sh
+++ locale/make_mo.sh
@@ -3,3 +3,4 @@
 while read pofile; do
         msgfmt --use-fuzzy -c -o ${pofile%po}mo $pofile || exit $?
 done < <(find $(dirname $0) -type f ! -wholename '*/.svn*' -name '*.po')
+msgfmt --use-fuzzy -c -o stools.mo sltools.po
