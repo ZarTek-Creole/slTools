a4547b0b203f5fd204240dcd364e125f9b7c3580
ZarTeK Creaole <zartek.creole@gmail.Com>
2022-04-15 18:54:47 +0000

    add eta

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ set -o errexit
 # exit on fail variable
 set -o nounset
 
+echo "SL-Tools Install"
 # intsall sltools 
 install -m 755 -d /opt/sltools
 install -m 755                          \
@@ -11,13 +12,13 @@ install -m 755                          \
     opt/sltools/sltools-core	        \
     opt/sltools/sltools-functions       \
     /opt/sltools
-
+echo "SL-Tools create default config file"
 # Create default conf
 install -m 755 -d /etc/slftp
 install -m 755                  \
     etc/slftp/sltools.cfg.dist  \
     /etc/slftp/
-
+echo "SL-Tools Create Symbolic Links"
 # create symbolic link
 [ -L /usr/bin/sltools ] && unlink /usr/bin/sltools
 ln -s /opt/sltools/sltools /usr/bin/
@@ -25,25 +26,19 @@ ln -s /opt/sltools/sltools /usr/bin/
 [ -L /usr/bin/sltools-core ] && unlink /usr/bin/sltools-core
 ln -s /opt/sltools/sltools-core /usr/bin/
 
+echo "SL-Tools copy internal dependecies"
 # install depend internal
 install -m 755 -d /opt/src
 cp -r opt/src/fpc /opt/src/
 cp -r opt/src/slftp /opt/src/
 
-# Init log files
+echo "SL-Tools initialisation log file"
 touch /var/log/sltools.log
 chmod 0666 /var/log/sltools.log
 
-#Lang install
+echo "SL-Tools install pack languages"
 for LOCALE in $(find locale/ -name sltools.mo | cut -d/ -f2); do
-	echo "Install locale : ${LOCALE}"
+	echo "Install language: ${LOCALE}"
 	install -Dm 644 "./locale/${LOCALE}/LC_MESSAGES/sltools.mo" "/usr/share/locale/${LOCALE}/LC_MESSAGES/"
 done
-echo "*-----------------------------------------*"
-echo "* SL-Tools a été installé                 *"
-echo "* le répertoire de configuration se       *"
-echo "* trouve dans /etc/slftp et doit être     *"
-echo "* renommé en sltools.cfg                  *"
-echo "* Après vous pouvez éxécuter le programme *"
-echo "* Avec la commande sltools (root/user)    *"
-echo "*-----------------------------------------*"
+echo "SL-Tools Successful installation"
